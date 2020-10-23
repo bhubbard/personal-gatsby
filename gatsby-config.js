d6167274b1cdc8f744f8fff5a44b27e45c4a2e9e
Brandon Hubbard <bhubbard@users.noreply.github.com>
2020-10-22 21:10:50 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Brandon Hubbard`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Brandon Hubbard`,
+      summary: `Random things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    siteUrl: `https://bhubbard.gtsb.io/`,
     social: {
       twitter: `kylemathews`,
     },
